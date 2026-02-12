15cec86e389c6b2513f25c08783c3a3eb50f8804
Varriable <ramadhanabdilatif13@gmail.com>
2026-02-12 23:11:19 +0300

    seletors removed and services added

---
+++ backend/services/otp_service.py
@@ -0,0 +1,21 @@
+from ..apps.secret.models import Otp
+
+class OtpService:
+    @staticmethod
+    def create_otp(user, secret) -> Otp:
+        code = Otp.generate_code()
+        otp = Otp.objects.create(user=user, secret=secret, code=code)
+        return otp
+        
+    @staticmethod
+    def get_user_otps(user) -> list[Otp]:
+        otps = Otp.objects.filter(user=user) 
+        return otps
+    
+    @staticmethod
+    def get_otp_by_id(otp_id: int) -> Otp: 
+        try: 
+            otp = Otp.objects.get(id=otp_id)
+            return otp
+        except Otp.DoesNotExist:
+            raise ValueError("OTP not found")
---
+++ backend/services/user_service.py
@@ -0,0 +1,28 @@
+from ..apps.user.models import User
+
+class UserService:
+    @staticmethod
+    def create_user(email: str, password: str) -> User:
+        user = User.objects.create_user(email=email, password=password) 
+        return user
+    
+    @staticmethod
+    def edit_user(user_id: int, email: str = None, password: str = None) -> User:
+        try:
+            user = User.objects.get(id=user_id)
+            if email: user.email = email 
+            if password: user.set_password(password) 
+            user.save() 
+            return user 
+        except User.DoesNotExist: 
+            raise ValueError("User not found")
+    
+
+    @staticmethod
+    def delete_user(user_id: int) -> None:
+        try: 
+            user = User.objects.get(id=user_id) 
+            user.is_active = False
+            user.save()
+        except User.DoesNotExist: 
+            raise ValueError("User not found")
